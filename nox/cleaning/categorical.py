bf18ad6f81957ec6d0c2f985f58fd8b72136495e
Karthik Bhaskar <karthikrajabk@gmail.com>
2021-05-28 08:42:41 -0400

    added docstring for default category

--- nox/cleaning/categorical.py
+++ nox/cleaning/categorical.py
@@ -3,7 +3,14 @@ from nox.util import _get_columns
 
 
 def _determine_default_category(x_train, col, replacement_categories):
-
+    
+    """
+    A utility function to help determine the default category name for a column that has missing
+    categorical values. 
+    
+    It takes in a list of possible values and if any the first value in the list
+    that is not a value in the column is the category that will be used to replace missing values.
+    """
 
     unique_vals_col = x_train[col].unique()
     for potential_category in replacement_categories:
