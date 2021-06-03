5f60a4feb1e8794eaef6ae4e8fef7967922b5ad5
Karthik Bhaskar <karthikrajabk@gmail.com>
2021-06-03 05:26:40 -0400

    Added doc_string for drop_column_missing_threshold

--- nox/cleaning/clean.py
+++ nox/cleaning/clean.py
@@ -12,8 +12,26 @@ from nox.util import _input_columns, _numeric_input_conditions
 # Create cleaning code by creating different func
 class Clean(object):
 
-    
+
     def drop_column_missing_threshold(self, threshold: float):
+
+        """
+        Remove columns from the dataframe that have greater than or equal to the threshold value of missing values.
+        Example: Remove columns where >= 50% of the data is missing.
+        
+        Parameters
+        ----------
+        threshold : float
+            Value between 0 and 1 that describes what percentage of a column can be missing values.
+        
+        Returns
+        -------
+        Data:
+            Returns a deep copy of the Data object.
+        Examples
+        --------
+        >>> data.drop_column_missing_threshold(0.5)
+        """
 
         if threshold > 1 or threshold < 0:
             raise ValueError("Threshold cannot be greater than 1 or less than 0.")
