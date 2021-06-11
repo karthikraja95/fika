45f8df87b6b1ef673d5a80f23573d751f8d9cf35
Karthik Bhaskar <karthikrajabk@gmail.com>
2021-06-11 09:33:14 -0400

    docstring  replace_missing_interpolate

--- nyx/cleaning/clean.py
+++ nyx/cleaning/clean.py
@@ -565,6 +565,48 @@ class Clean(object):
         self, *list_args, list_of_cols=[], method="linear", **inter_kwargs
     ):
 
+        """
+        Replaces missing values with an interpolation method and possible extrapolation.
+        The possible interpolation methods are:
+           
+            - 'linear': Ignore the index and treat the values as equally spaced. This is the only method supported on MultiIndexes.
+            - 'time': Works on daily and higher resolution data to interpolate given length of interval.
+            - 'index', ‘values’: use the actual numerical values of the index.
+            - 'pad': Fill in NaNs using existing values.
+            - 'nearest', 'zero', 'slinear', 'quadratic', 'cubic', 'spline', ‘barycentric’, ‘polynomial’: Passed to scipy.interpolate.interp1d.
+                - These methods use the numerical values of the index. Both ‘polynomial’ and ‘spline’ require that you also specify an order (int), e.g. df.interpolate(method='polynomial', order=5).
+            - 'krogh', 'piecewise_polynomial', 'spline', 'pchip', 'akima': Wrappers around the SciPy interpolation methods of similar names.
+            - 'from_derivatives': Refers to scipy.interpolate.BPoly.from_derivatives which replaces ‘piecewise_polynomial’ interpolation method in scipy 0.18.
+        For more information see: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.Series.interpolate.html or https://docs.scipy.org/doc/scipy/reference/interpolate.html.
+        Parameters
+        ----------
+        list_args : str(s), optional
+            Specific columns to apply this technique to.
+            
+        list_of_cols : list, optional
+            A list of specific columns to apply this technique to., by default []
+        method : str, optional
+            Interpolation method, by default 'linear'
+        limit : int, optional
+            Maximum number of consecutive NaNs to fill. Must be greater than 0.
+        limit_area : {None, ‘inside’, ‘outside’}, default None
+            If limit is specified, consecutive NaNs will be filled with this restriction.
+            - None: No fill restriction.
+            - ‘inside’: Only fill NaNs surrounded by valid values (interpolate).
+            - ‘outside’: Only fill NaNs outside valid values (extrapolate).
+        
+        Returns
+        -------
+        Data:
+            Returns a deep copy of the Data object.
+        Examples
+        --------
+        >>> data.replace_missing_interpolate('col1', 'col2')
+        >>> data.replace_missing_interpolate(['col1', 'col2'])
+        >>> data.replace_missing_interpolate('col1', 'col2', method='pad', limit=3)
+        """
+
+
         method = inter_kwargs.pop("method", "linear")
         list_of_cols = _input_columns(list_args, list_of_cols)
 
