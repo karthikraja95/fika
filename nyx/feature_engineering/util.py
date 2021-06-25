a1dcb28d22a03c49f635d916fc0cd7393c084b5c
Karthik Bhaskar <karthikrajabk@gmail.com>
2021-06-25 12:28:38 +0530

    dimentionality reduction function

--- nyx/feature_engineering/util.py
+++ nyx/feature_engineering/util.py
@@ -1,3 +1,22 @@
 import pandas as pd
 from sklearn.decomposition import PCA, TruncatedSVD
 
+def sklearn_dim_reduction(
+    x_train, x_test=None, algo=None, n_components=50, **dim_reduce_kwargs
+):
+
+    algorithms = {
+        "pca": PCA(n_components=n_components, **dim_reduce_kwargs),
+        "tsvd": TruncatedSVD(n_components=n_components, **dim_reduce_kwargs),
+    }
+
+    reducer = algorithms[algo]
+
+    x_train = pd.DataFrame(reducer.fit_transform(x_train))
+    x_train.columns = map(str, x_train.columns)
+
+    if x_test is not None:
+        x_test = pd.DataFrame(reducer.transform(x_test))
+        x_test.columns = map(str, x_test.columns)
+
+    return x_train, x_test
