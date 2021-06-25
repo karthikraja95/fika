ed8419d32fb22b88f089997542f0f97b0a191347
Karthik Bhaskar <karthikrajabk@gmail.com>
2021-06-25 12:29:13 +0530

    docstring for sklearn_dim_reduction function

--- nyx/feature_engineering/util.py
+++ nyx/feature_engineering/util.py
@@ -5,6 +5,25 @@ def sklearn_dim_reduction(
     x_train, x_test=None, algo=None, n_components=50, **dim_reduce_kwargs
 ):
 
+    """
+    Performs Principal Component Analysis on a dataset.
+    
+    Parameters
+    ----------
+    x_train : DataFrame
+        Dataset
+    n_components : Any
+        Number of components to reduce to - algo dependent.
+    x_test : DataFrame
+        Testing dataset, by default None
+    
+    Returns
+    -------
+    Dataframe, *Dataframe
+        Transformed dataframe with the new column
+    Returns 2 Dataframes if x_test is provided. 
+    """
+
     algorithms = {
         "pca": PCA(n_components=n_components, **dim_reduce_kwargs),
         "tsvd": TruncatedSVD(n_components=n_components, **dim_reduce_kwargs),
