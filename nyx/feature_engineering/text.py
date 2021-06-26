012d6564c4d75d59e2e54d91ea82b353d2c0ef9c
Karthik Bhaskar <karthikrajabk@gmail.com>
2021-06-26 06:35:42 +0530

    docstring for textblob  features

--- nyx/feature_engineering/text.py
+++ nyx/feature_engineering/text.py
@@ -10,6 +10,30 @@ def textblob_features(
     x_train, x_test, feature, list_of_cols=[], new_col_name="_postagged",
 ):
 
+    """
+    Part of Speech tag the text data provided. Used to tag each word as a Noun, Adjective,
+    Verbs, etc.
+    This utilizes TextBlob which utlizes the NLTK tagger and is a wrapper for the tagging process.
+    
+    Parameters
+    ----------
+    x_train : DataFrame
+        Dataset
+    x_test : DataFrame
+        Testing dataset, by default None
+    feature : str,
+        Textblob feature
+    list_of_cols : list, optional
+        A list of specific columns to apply this technique to, by default []
+    new_col_name : str, optional
+        New column name to be created when applying this technique, by default `COLUMN_postagged`
+    Returns
+    -------
+    Dataframe, *Dataframe
+        Transformed dataframe with the new column.
+    Returns 2 Dataframes if x_test is provided. 
+    """
+
     list_of_cols = _get_columns(list_of_cols, x_train)
 
     for col in list_of_cols:
