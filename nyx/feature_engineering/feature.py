a5745e7fbc88a0c1f301b5a04daf871aaabfdad2
Karthik Bhaskar <karthikrajabk@gmail.com>
2021-06-27 06:33:51 +0530

    docstring parameters for one hot encode

--- nyx/feature_engineering/feature.py
+++ nyx/feature_engineering/feature.py
@@ -30,6 +30,33 @@ class Feature(object):
         For more info see: https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.OneHotEncoder.html
         If a list of columns is provided use the list, otherwise use arguments.
 
+        Parameters
+        ----------
+        list_args : str(s), optional
+            Specific columns to apply this technique to.
+        list_of_cols : list, optional
+            A list of specific columns to apply this technique to., by default []
+        keep_col : bool
+            A parameter to specify whether to drop the column being transformed, by default
+            keep the column, True
+        categories : ‘auto’ or a list of array-like, default=’auto’
+            Categories (unique values) per feature:
+                ‘auto’ : Determine categories automatically from the training data.
+                list : categories[i] holds the categories expected in the ith column. The passed categories should not mix strings and numeric values within a single feature, and should be sorted in case of numeric values.
+            The used categories can be found in the categories_ attribute.
+        drop : ‘first’ or a array-like of shape (n_features,), default=None
+            Specifies a methodology to use to drop one of the categories per feature. This is useful in situations where perfectly collinear features cause problems, such as when feeding the resulting data into a neural network or an unregularized regression.
+                None : retain all features (the default).
+                ‘first’ : drop the first category in each feature. If only one category is present, the feature will be dropped entirely.
+                array : drop[i] is the category in feature X[:, i] that should be dropped.
+        sparsebool : default=True
+            Will return sparse matrix if set True else will return an array.
+        dtype : number type, default=np.float
+            Desired dtype of output.
+        handle_unknown: {‘error’, ‘ignore’}, default='ignore'
+            Whether to raise an error or ignore if an unknown categorical feature is present during transform (default is to raise).
+            When this parameter is set to ‘ignore’ and an unknown category is encountered during transform, the resulting one-hot encoded columns for this feature will be all zeros.
+            In the inverse transform, an unknown category will be denoted as None.
 
         Returns
         -------
