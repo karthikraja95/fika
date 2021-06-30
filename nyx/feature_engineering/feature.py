8a20268314bcb875e2f995e4ca6a5498f7f217df
Karthik Bhaskar <karthikrajabk@gmail.com>
2021-06-30 06:39:39 +0530

    docstring postag_spacy_detailed

--- nyx/feature_engineering/feature.py
+++ nyx/feature_engineering/feature.py
@@ -453,6 +453,29 @@ class Feature(object):
         self, *list_args, list_of_cols=[], new_col_name="_postagged"
     ):
 
+        """
+        Tag documents with their respective "Part of Speech" tag with the Spacy NLP engine and the PennState PoS tags.
+        These tags classify a word as a noun, verb, adjective, etc. A full list and their meaning can be found here:
+        https://spacy.io/api/annotation#pos-tagging 
+        If a list of columns is provided use the list, otherwise use arguments.
+        
+        Parameters
+        ----------
+        list_args : str(s), optional
+            Specific columns to apply this technique to.
+        list_of_cols : list, optional
+            A list of specific columns to apply this technique to., by default []
+        new_col_name : str, optional
+            New column name to be created when applying this technique, by default `COLUMN_postagged`
+        Returns
+        -------
+        Data:
+            Returns a deep copy of the Data object.
+        Examples
+        --------
+        >>> data.postag_spacy_detailed('col1', 'col2', 'col3')
+        """
+
         list_of_cols = _input_columns(list_args, list_of_cols)
 
         (self.x_train, self.x_test,) = text.spacy_feature_postag(
