713a51983ef4b9a9bbada6240869f09da47e1745
Karthik <karthikrajabk@gmail.com>
2021-07-21 17:45:31 -0400

    plot_colors done

--- nyx/visualizations/visualizations.py
+++ nyx/visualizations/visualizations.py
@@ -1,3 +1,20 @@
 from nyx.visualizations.visualize import VizCreator
 import numpy as np
 import pandas as pd
+
+class Visualizations(object):
+    @property
+    def _viz(self):
+        return VizCreator()
+
+    @property
+    def plot_colors(self):  # pragma: no cover
+        """
+        Displays all plot colour names
+        """
+
+        from IPython.display import IFrame
+
+        IFrame(
+            "https://python-graph-gallery.com/wp-content/uploads/100_Color_names_python.png"
+        )
