80125199c79a84eb93badfebdd39e76fa69c0191
Karthik <karthikrajabk@gmail.com>
2021-07-22 19:09:56 -0400

    add_to_queue function done

--- nyx/modelling/util.py
+++ nyx/modelling/util.py
@@ -26,3 +26,30 @@ from nyx.config import EXP_DIR, DEFAULT_MODEL_DIR, IMAGE_DIR, cfg
 from nyx.config.config import _global_config
 from nyx.util import _make_dir
 from pickle import dump
+
+def add_to_queue(model_function):
+    @wraps(model_function)
+    def wrapper(self, *args, **kwargs):
+        default_kwargs = get_default_args(model_function)
+
+        kwargs["run"] = kwargs.get("run", default_kwargs["run"])
+        kwargs["model_name"] = kwargs.get("model_name", default_kwargs["model_name"])
+        cv = kwargs.get("cv", False)
+
+        if not _validate_model_name(self, kwargs["model_name"]):
+            raise AttributeError("Invalid model name. Please choose another one.")
+
+        if kwargs["run"]:
+            return model_function(self, *args, **kwargs)
+        else:
+            if "XGBoost" in model_function.__name__:
+                print(
+                    "XGBoost is not comptabile to be run in parallel. Please run it normally or in run all models in a series."
+                )
+            else:
+                kwargs["run"] = True
+                self._queued_models[kwargs["model_name"]] = partial(
+                    getattr(self, model_function.__name__), *args, **kwargs
+                )
+
+    return wrapper
