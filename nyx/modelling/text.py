fadb881d9b70f07a3f9f5c49584a1d4c3d075c2c
Karthik <karthikrajabk@gmail.com>
2021-07-25 11:52:38 -0400

    text rank summarizer

--- nyx/modelling/text.py
+++ nyx/modelling/text.py
@@ -6,3 +6,43 @@ from gensim.summarization.summarizer import summarize
 from nltk.tokenize import word_tokenize
 
 from nyx.preprocessing.text import process_text
+
+def gensim_textrank_summarizer(
+    x_train, x_test=None, list_of_cols=[], new_col_name="_summarized", **algo_kwargs
+):
+    """
+    Uses Gensim Text Rank summarize to extractively summarize text.
+    Note this uses a variant of Text Rank.
+    
+    Parameters
+    ----------
+    x_train : DataFrame
+        Dataset
+    x_test : DataFrame
+        Testing dataset, by default None
+    list_of_cols : list, optional
+        Column name(s) of text data that you want to summarize
+    new_col_name : str, optional
+        New column name to be created when applying this technique, by default `_summarized`
+    
+    Returns
+    -------
+    Dataframe, *Dataframe
+        Transformed dataframe with the new column.
+    Returns 2 Dataframes if x_test is provided. 
+    """
+
+    for col in list_of_cols:
+        if new_col_name.startswith("_"):
+            new_col_name = col + new_col_name
+
+        x_train.loc[:, new_col_name] = [
+            summarize(x, **algo_kwargs) for x in x_train[col]
+        ]
+
+        if x_test is not None:
+            x_test.loc[:, new_col_name] = [
+                summarize(x, **algo_kwargs) for x in x_test[col]
+            ]
+
+    return x_train, x_test
