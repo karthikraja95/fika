b1a1165b9b8e3f2b59d5c708bd74defabaa25649
Karthik <karthikrajabk@gmail.com>
2021-07-25 11:53:20 -0400

    text rank keywords done

--- nyx/modelling/text.py
+++ nyx/modelling/text.py
@@ -46,3 +46,48 @@ def gensim_textrank_summarizer(
             ]
 
     return x_train, x_test
+
+    def gensim_textrank_keywords(
+    x_train,
+    x_test=None,
+    list_of_cols=[],
+    new_col_name="_extracted_keywords",
+    **algo_kwargs
+):
+    """
+    Uses Gensim Text Rank summarize to extract keywords.
+    Note this uses a variant of Text Rank.
+    
+    Parameters
+    ----------
+    x_train : DataFrame
+        Dataset
+    x_test : DataFrame
+        Testing dataset, by default None
+    list_of_cols : list, optional
+        Column name(s) of text data that you want to summarize
+    new_col_name : str, optional
+        New column name to be created when applying this technique, by default `_extracted_keywords`
+    
+    Returns
+    -------
+    Dataframe, *Dataframe
+        Transformed dataframe with the new column.
+    Returns 2 Dataframes if x_test is provided. 
+    """
+
+    for col in list_of_cols:
+
+        if new_col_name.startswith("_"):
+            new_col_name = col + new_col_name
+
+        x_train.loc[:, new_col_name] = [
+            keywords(x, **algo_kwargs) for x in x_train[col]
+        ]
+
+        if x_test is not None:
+            x_test.loc[:, new_col_name] = [
+                keywords(x, **algo_kwargs) for x in x_test[col]
+            ]
+
+    return x_train, x_test
