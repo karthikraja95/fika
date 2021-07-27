07331b025906713fcf68a2f45fdcb77dae3b9c8a
Karthik <karthikrajabk@gmail.com>
2021-07-27 16:31:50 -0400

    generate_service @classmethod done

--- nyx/templates/template_generator.py
+++ nyx/templates/template_generator.py
@@ -21,3 +21,43 @@ class TemplateGenerator(object):
     )
 
     project_dir = _create_dir()
+
+    @classmethod
+    def generate_service(cls, name: str, filename: str, model):
+        """
+        Generates the necessary files to run your model as a service.
+        Generates the app.py, Dockerfile and requirements.txt file.
+        
+        Parameters
+        ----------
+        name : str
+            Project name
+        filename : str
+            Model file name
+        """
+
+        _create_project_dir(cls.project_dir, name=name)
+
+        files = ["main.py", "Dockerfile", "requirements.txt"]
+        model_kwargs = _get_model_type_kwarg(model)
+
+        for file in files:
+            script = cls.env.get_template("files/" + file.replace(".py", "")).render(
+                name=name, filename=filename, service=True, **model_kwargs,
+            )
+
+            if file.endswith(".py") or file.endswith(".txt"):
+                with open(
+                    os.path.join(cls.project_dir, name, "app", file),
+                    "w",
+                    encoding="utf8",
+                ) as f:
+                    f.write(script)
+            else:
+                with open(
+                    os.path.join(cls.project_dir, name, file), "w", encoding="utf8"
+                ) as f:
+                    f.write(script)
+
+        print(f"Deployment files can be found at {cls.project_dir}/{name}.")
+        print()
