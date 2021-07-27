b7b913b36d392bd5208bdb94949f6212fa8f9a2e
Karthik <karthikrajabk@gmail.com>
2021-07-27 16:27:09 -0400

    create dir done

--- nyx/templates/util.py
+++ nyx/templates/util.py
@@ -2,3 +2,25 @@ import os
 
 from nyx.config import DEFAULT_DEPLOYMENTS_DIR, cfg
 from nyx.util import _make_dir
+
+
+def _create_dir():
+    """
+    Creates the projects directory.
+    
+    Parameters
+    ----------
+    project_dir : str
+        Full path of the project dir.
+    name : str
+        Name of the project
+    """
+
+    if not cfg["models"]["deployment_dir"]:
+        dep_dir = DEFAULT_DEPLOYMENTS_DIR
+    else:
+        dep_dir = cfg["models"]["deployment_dir"]
+
+    _make_dir(dep_dir)
+
+    return dep_dir
