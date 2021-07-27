5949bb9ab15c64d92ec209298f443264c47664f6
Karthik <karthikrajabk@gmail.com>
2021-07-27 16:31:12 -0400

    TemplateGenerator class

--- nyx/templates/template_generator.py
+++ nyx/templates/template_generator.py
@@ -1,3 +1,23 @@
 import os
 import subprocess
 from pathlib import Path
+
+
+from jinja2 import Environment, PackageLoader
+
+from nyx.templates.util import (
+    _create_dir,
+    _create_project_dir,
+    _get_model_type_kwarg,
+)
+
+class TemplateGenerator(object):
+
+    # Prepare environment and source data
+    env = Environment(
+        loader=PackageLoader("aethos", "templates"),
+        trim_blocks=True,
+        lstrip_blocks=True,
+    )
+
+    project_dir = _create_dir()
