dcc4995e1a560550cbb91f7b85735aeee5a0ec6f
Karthik <karthikrajabk@gmail.com>
2021-08-03 16:04:37 -0400

    RegressionModelAnalysis

--- nyx/model_analysis/regression_model_analysis.py
+++ nyx/model_analysis/regression_model_analysis.py
@@ -7,3 +7,33 @@ import math
 from nyx.config.config import _global_config
 from .model_analysis import SupervisedModelAnalysis
 
+class RegressionModelAnalysis(SupervisedModelAnalysis):
+    def __init__(
+        self, model, x_train, x_test, target, model_name,
+    ):
+        """
+        Class to analyze Regression models through metrics, global/local interpretation and visualizations.
+        Parameters
+        ----------
+        model : str or Model Object
+            Sklearn, XGBoost, LightGBM Model object or .pkl file of the objects.
+        x_train : pd.DataFrame
+            Training Data used for the model.
+        x_test : pd.DataFrame
+            Test data used for the model.
+        target : str
+            Target column in the DataFrame
+        model_name : str
+            Name of the model for saving images and model tracking purposes
+        """
+
+        # TODO: Add check for pickle file
+
+        super().__init__(
+            model,
+            x_train.drop([target], axis=1),
+            x_test.drop([target], axis=1),
+            x_train[target],
+            x_test[target],
+            model_name,
+        )
