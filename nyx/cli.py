1bc3575848781db7c19b54b82ccd6434d463884e
Karthik <karthikrajabk@gmail.com>
2021-08-09 09:08:56 -0400

    install corpora done

--- nyx/cli.py
+++ nyx/cli.py
@@ -24,3 +24,34 @@ def enable_extensions():
     """
     os.system("jupyter nbextension enable --py --sys-prefix widgetsnbextension")
     os.system("jupyter nbextension enable --py --sys-prefix qgrid")
+
+@main.command()
+def install_corpora():
+    """
+    Installs the necessary corpora from spaCy and NLTK for NLP analysis.
+    """
+
+    py_exe = ""
+
+    try:
+        if "3." in subprocess.check_output(
+            "python --version", stderr=subprocess.STDOUT, shell=True
+        ).decode("utf-8"):
+            py_exe = "python"
+    except subprocess.CalledProcessError as e:
+        pass
+
+    try:
+        subprocess.check_output(
+            "python3 --version", stderr=subprocess.STDOUT, shell=True
+        ).decode("utf-8")
+        py_exe = "python3"
+    except subprocess.CalledProcessError as e:
+        pass
+
+    if not py_exe:
+        raise EnvironmentError("Python is not in your path, please add it your path.")
+
+    os.system(f"{py_exe} -m textblob.download_corpora")
+    os.system(f"{py_exe} -c 'import nltk; nltk.download(\"stopwords\")'")
+    os.system(f"{py_exe} -m spacy download en")
