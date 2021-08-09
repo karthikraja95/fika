9c7148b30306f9741761ebbe36d67cd8aebb8068
Karthik <karthikrajabk@gmail.com>
2021-08-09 09:09:18 -0400

    option, mlflow ui done

--- nyx/cli.py
+++ nyx/cli.py
@@ -55,3 +55,35 @@ def install_corpora():
     os.system(f"{py_exe} -m textblob.download_corpora")
     os.system(f"{py_exe} -c 'import nltk; nltk.download(\"stopwords\")'")
     os.system(f"{py_exe} -m spacy download en")
+
+@main.command()
+@click.option(
+    "-h",
+    "--host",
+    show_default=True,
+    default="0.0.0.0",
+    help="IP to bind to, default 0.0.0.0.",
+)
+@click.option(
+    "-p",
+    "--port",
+    show_default=True,
+    default="10000",
+    help="Port to bind to, default 10000.",
+)
+def mlflow_ui(host, port):
+    """
+    Starts the MLFlow UI locally. If you are running MLFlow remotely, please start it there.
+    """
+
+    if not EXP_DIR.startswith("file:"):
+        click.echo(
+            "If you are running MLFlow remotely, please start it on the remote server."
+        )
+        click.echo(
+            "If you are trying to run MLFlow locally, please the path starts like `file:/`."
+        )
+
+    os.system(
+        f"mlflow ui -h {host} -p {port} --backend-store-uri {EXP_DIR[5:]} --default-artifact-root {EXP_DIR[5:]}"
+    )
