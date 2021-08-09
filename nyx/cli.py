3218712bdd070709492593c25446a45a51c114af
Karthik <karthikrajabk@gmail.com>
2021-08-09 09:08:28 -0400

    main, create, enable extenstion done

--- nyx/cli.py
+++ nyx/cli.py
@@ -3,3 +3,24 @@ import os
 import click
 import subprocess
 from nyx.config.user_config import EXP_DIR
+
+@click.group()
+def main():
+    pass
+
+
+@main.command()
+def create():
+    """
+    Creates a Data Science folder structure and files.
+    """
+    os.system("cookiecutter https://github.com/drivendata/cookiecutter-data-science")
+
+
+@main.command()
+def enable_extensions():
+    """
+    Enables jupyter extensions such as qgrid.
+    """
+    os.system("jupyter nbextension enable --py --sys-prefix widgetsnbextension")
+    os.system("jupyter nbextension enable --py --sys-prefix qgrid")
