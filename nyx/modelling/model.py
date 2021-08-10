57644d152b7a071224eeaa59643b115321aa48de
Karthik <karthikrajabk@gmail.com>
2021-08-10 08:38:23 -0400

    BaseModel instantiation done

--- nyx/modelling/model.py
+++ nyx/modelling/model.py
@@ -30,3 +30,33 @@ from nyx.templates.template_generator import TemplateGenerator as tg
 from nyx.util import _input_columns, split_data, _get_attr_, _get_item_
 
 warnings.simplefilter("ignore", FutureWarning)
+
+class ModelBase(object):
+    def __init__(
+        self,
+        x_train,
+        target,
+        x_test=None,
+        test_split_percentage=0.2,
+        exp_name="my-experiment",
+    ):
+
+        self._models = {}
+        self._queued_models = {}
+        self.exp_name = exp_name
+
+        problem = "c" if type(self).__name__ == "Classification" else "r"
+
+        self.x_train = x_train
+        self.x_test = x_test
+        self.target = target
+        self.test_split_percentage = test_split_percentage
+        self.target_mapping = None
+
+        if self.x_test is None and not type(self).__name__ == "Unsupervised":
+            # Generate train set and test set.
+            self.x_train, self.x_test = split_data(
+                self.x_train, test_split_percentage, self.target, problem
+            )
+            self.x_train = self.x_train.reset_index(drop=True)
+            self.x_test = self.x_test.reset_index(drop=True)
