10e92f18cdee3b0c7f05268f16c4fc7049d18c9b
Karthik <karthikrajabk@gmail.com>
2021-08-11 15:36:24 -0400

    Decision Tree Classifier done

--- nyx/modelling/classification_models.py
+++ nyx/modelling/classification_models.py
@@ -1114,3 +1114,142 @@ class Classification(
         )
 
         return model
+
+    @add_to_queue
+    def DecisionTreeClassification(
+        self,
+        cv_type=None,
+        gridsearch=None,
+        score="accuracy",
+        model_name="dt_cls",
+        run=True,
+        verbose=1,
+        **kwargs,
+    ):
+        # region
+        """
+        Trains a Decision Tree classification model.
+        For more Decision Tree info, you can view it here: https://scikit-learn.org/stable/modules/generated/sklearn.tree.DecisionTreeClassifier.html#sklearn.tree.DecisionTreeClassifier
+        If running gridsearch, the implemented cross validators are:
+            - 'kfold' for KFold
+            - 'strat-kfold' for StratifiedKfold
+        Possible scoring metrics: 
+            - ‘accuracy’ 	
+            - ‘balanced_accuracy’ 	
+            - ‘average_precision’ 	
+            - ‘brier_score_loss’ 	
+            - ‘f1’ 	
+            - ‘f1_micro’ 	
+            - ‘f1_macro’ 	
+            - ‘f1_weighted’ 	
+            - ‘f1_samples’ 	
+            - ‘neg_log_loss’ 	
+            - ‘precision’	
+            - ‘recall’ 	
+            - ‘jaccard’ 	
+            - ‘roc_auc’
+        
+        Parameters
+        ----------
+        cv_type : bool, optional
+            If True run crossvalidation on the model, by default None.
+        gridsearch : int, Crossvalidation Generator, optional
+            Cross validation method, by default None
+        score : str, optional
+            Scoring metric to evaluate models, by default 'accuracy'
+        model_name : str, optional
+            Name for this model, by default "dt_cls"
+        run : bool, optional
+            Whether to train the model or just initialize it with parameters (useful when wanting to test multiple models at once) , by default False
+        verbose : int, optional
+            Verbosity level of model output, the higher the number - the more verbose. By default, 1   
+        	
+        criterion : string, optional (default=”gini”)
+            The function to measure the quality of a split.
+            Supported criteria are “gini” for the Gini impurity and “entropy” for the information gain.
+        splitter : string, optional (default=”best”)
+            The strategy used to choose the split at each node.
+            Supported strategies are “best” to choose the best split and “random” to choose the best random split.
+        max_depth : int or None, optional (default=None)
+            The maximum depth of the tree.
+            If None, then nodes are expanded until all leaves are pure or until all leaves contain less than min_samples_split samples.
+        min_samples_split : int, float, optional (default=2)
+            The minimum number of samples required to split an internal node:
+                If int, then consider min_samples_split as the minimum number.
+                If float, then min_samples_split is a fraction and ceil(min_samples_split * n_samples) are the minimum number of samples for each split.
+        min_samples_leaf : int, float, optional (default=1)
+            The minimum number of samples required to be at a leaf node. A split point at any depth will only be considered if it leaves at least min_samples_leaf training samples in each of the left and right branches. This may have the effect of smoothing the model, especially in regression.
+                If int, then consider min_samples_leaf as the minimum number.
+                If float, then min_samples_leaf is a fraction and ceil(min_samples_leaf * n_samples) are the minimum number of samples for each node.
+        max_features : int, float, string or None, optional (default=None)
+            The number of features to consider when looking for the best split:
+                    If int, then consider max_features features at each split.
+                    If float, then max_features is a fraction and int(max_features * n_features) features are considered at each split.
+                    If “auto”, then max_features=sqrt(n_features).
+                    If “sqrt”, then max_features=sqrt(n_features).
+                    If “log2”, then max_features=log2(n_features).
+                    If None, then max_features=n_features.
+            Note: the search for a split does not stop until at least one valid partition of the node samples is found, even if it requires to effectively inspect more than max_features features.
+        max_leaf_nodes : int or None, optional (default=None)
+            Grow a tree with max_leaf_nodes in best-first fashion.
+            Best nodes are defined as relative reduction in impurity.
+            If None then unlimited number of leaf nodes.
+        min_impurity_decrease : float, optional (default=0.)
+            A node will be split if this split induces a decrease of the impurity greater than or equal to this value.
+            The weighted impurity decrease equation is the following:
+            N_t / N * (impurity - N_t_R / N_t * right_impurity
+                                - N_t_L / N_t * left_impurity)
+            where N is the total number of samples, N_t is the number of samples at the current node, N_t_L is the number of samples in the left child, and N_t_R is the number of samples in the right child.
+            N, N_t, N_t_R and N_t_L all refer to the weighted sum, if sample_weight is passed.
+        min_impurity_split : float, (default=1e-7)
+            Threshold for early stopping in tree growth.
+            A node will split if its impurity is above the threshold, otherwise it is a leaf.
+        class_weight : dict, list of dicts, “balanced” or None, default=None
+            Weights associated with classes in the form {class_label: weight}.
+            If not given, all classes are supposed to have weight one.
+            For multi-output problems, a list of dicts can be provided in the same order as the columns of y.
+            Note that for multioutput (including multilabel) weights should be defined for each class of every column in its own dict.
+            For example, for four-class multilabel classification weights should be [{0: 1, 1: 1}, {0: 1, 1: 5}, {0: 1, 1: 1}, {0: 1, 1: 1}] instead of [{1:1}, {2:5}, {3:1}, {4:1}].
+            The “balanced” mode uses the values of y to automatically adjust weights inversely proportional to class frequencies in the input data as n_samples / (n_classes * np.bincount(y))
+            For multi-output, the weights of each column of y will be multiplied.
+            Note that these weights will be multiplied with sample_weight (passed through the fit method) if sample_weight is specified.
+        presort : bool, optional (default=False)
+            Whether to presort the data to speed up the finding of best splits in fitting.
+            For the default settings of a decision tree on large datasets, setting this to true may slow down the training process.
+            When using either a smaller dataset or a restricted depth, this may speed up the training.
+        ccp_alphanon-negative : float, optional (default=0.0)
+            Complexity parameter used for Minimal Cost-Complexity Pruning.
+            The subtree with the largest cost complexity that is smaller than ccp_alpha will be chosen.
+            By default, no pruning is performed.
+            See Minimal Cost-Complexity Pruning for details.
+        Returns
+        -------
+        ClassificationModelAnalysis
+            ClassificationModelAnalysis object to view results and analyze results
+        Examples
+        --------
+        >>> model.DecisionTreeClassification()
+        >>> model.DecisionTreeClassification(model_name='m1', min_impurity_split=0.0003)
+        >>> model.DecisionTreeClassification(cv_type='kfold')
+        >>> model.DecisionTreeClassification(gridsearch={'min_impurity_split':[0.01, 0.02]}, cv_type='strat-kfold')
+        >>> model.DecisionTreeClassification(run=False) # Add model to the queue
+        """
+        # endregion
+
+        from sklearn.tree import DecisionTreeClassifier
+
+        model = DecisionTreeClassifier
+
+        model = self._run_supervised_model(
+            model,
+            model_name,
+            ClassificationModelAnalysis,
+            cv_type=cv_type,
+            gridsearch=gridsearch,
+            score=score,
+            run=run,
+            verbose=verbose,
+            **kwargs,
+        )
+
+        return model
