7ef0794030d4ffc4977580e04d76c24fb3132772
Karthik <karthikrajabk@gmail.com>
2021-08-12 09:20:06 -0400

    Regression Class

--- nyx/modelling/regression_models.py
+++ nyx/modelling/regression_models.py
@@ -10,3 +10,38 @@ from nyx.feature_engineering.feature import Feature
 from nyx.visualizations.visualizations import Visualizations
 from nyx.stats.stats import Stats
 from nyx.modelling.util import add_to_queue
+
+class Regression(
+    ModelBase, Analysis, Clean, Preprocess, Feature, Visualizations, Stats
+):
+    def __init__(
+        self,
+        x_train,
+        target,
+        x_test=None,
+        test_split_percentage=0.2,
+        exp_name="my-experiment",
+    ):
+        """
+        Class to run analysis, transform your data and run Regression algorithms.
+        Parameters
+        -----------
+        x_train: pd.DataFrame
+            Training data or aethos data object
+        target: str
+            For supervised learning problems, the name of the column you're trying to predict.
+        x_test: pd.DataFrame
+            Test data, by default None
+        test_split_percentage: float
+            Percentage of data to split train data into a train and test set, by default 0.2.
+        exp_name : str
+            Experiment name to be tracked in MLFlow.
+        """
+
+        super().__init__(
+            x_train,
+            target,
+            x_test=x_test,
+            test_split_percentage=test_split_percentage,
+            exp_name=exp_name,
+        )
