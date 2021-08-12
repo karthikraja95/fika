628e5a40aa3a25c428a9979f45a7c2012a35f92d
Karthik <karthikrajabk@gmail.com>
2021-08-12 09:20:28 -0400

    Linear Regression done

--- nyx/modelling/regression_models.py
+++ nyx/modelling/regression_models.py
@@ -45,3 +45,82 @@ class Regression(
             test_split_percentage=test_split_percentage,
             exp_name=exp_name,
         )
+
+    @add_to_queue
+    def LinearRegression(
+        self,
+        cv_type=None,
+        gridsearch=None,
+        score="neg_mean_squared_error",
+        model_name="lin_reg",
+        run=True,
+        verbose=1,
+        **kwargs,
+    ):
+        # region
+        """
+        Trains a Linear Regression.
+        For more Linear Regression info, you can view it here: https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LinearRegression.html#sklearn.linear_model.LinearRegression
+        If running gridsearch, the implemented cross validators are:
+            - 'kfold' for KFold
+            - 'strat-kfold' for StratifiedKfold
+        Possible scoring metrics: 
+            - ‘explained_variance’ 	 
+            - ‘max_error’ 	 
+            - ‘neg_mean_absolute_error’ --> MAE	 
+            - ‘neg_mean_squared_error’ --> MSE 	 
+            - ‘neg_mean_squared_log_error’ --> MSLE
+            - ‘neg_median_absolute_error’ --> MeAE 	 
+            - ‘r2’
+        
+        Parameters
+        ----------
+        cv : bool, optional
+            If True run crossvalidation on the model, by default None.
+        gridsearch : int, Crossvalidation Generator, optional
+            Cross validation method, by default None
+        score : str, optional
+            Scoring metric to evaluate models, by default ‘neg_mean_squared_error’
+        model_name : str, optional
+            Name for this model, by default "lin_reg"
+        run : bool, optional
+            Whether to train the model or just initialize it with parameters (useful when wanting to test multiple models at once) , by default False
+        verbose : int, optional
+            Verbosity level of model output, the higher the number - the more verbose. By default, 1        	
+        fit_intercept : boolean, optional, default True
+            whether to calculate the intercept for this model.
+            If set to False, no intercept will be used in calculations (e.g. data is expected to be already centered).
+        normalize : boolean, optional, default False
+            This parameter is ignored when fit_intercept is set to False.
+            If True, the regressors X will be normalized before regression by subtracting the mean and dividing by the l2-norm.
+        Returns
+        -------
+        RegressionModelAnalysis
+            RegressionModelAnalysis object to view results and analyze results
+        Examples
+        --------
+        >>> model.LinearRegression()
+        >>> model.LinearRegression(model_name='m1', normalize=True)
+        >>> model.LinearRegression(cv=10)
+        >>> model.LinearRegression(gridsearch={'normalize':[True, False]}, cv='strat-kfold')
+        >>> model.LinearRegression(run=False) # Add model to the queue
+        """
+        # endregion
+
+        from sklearn.linear_model import LinearRegression
+
+        model = LinearRegression
+
+        model = self._run_supervised_model(
+            model,
+            model_name,
+            RegressionModelAnalysis,
+            cv_type=cv_type,
+            gridsearch=gridsearch,
+            score=score,
+            run=run,
+            verbose=verbose,
+            **kwargs,
+        )
+
+        return model
