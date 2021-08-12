c8bca8ce4b0db2a48385479553bb8df6e7049878
Karthik <karthikrajabk@gmail.com>
2021-08-12 09:21:13 -0400

    Elastic Regresion done

--- nyx/modelling/regression_models.py
+++ nyx/modelling/regression_models.py
@@ -222,3 +222,112 @@ class Regression(
         )
 
         return model
+
+    @add_to_queue
+    def ElasticnetRegression(
+        self,
+        cv_type=None,
+        gridsearch=None,
+        score="neg_mean_squared_error",
+        model_name="elastic",
+        run=True,
+        verbose=1,
+        **kwargs,
+    ):
+        # region
+        """
+        Elastic Net regression with combined L1 and L2 priors as regularizer.
+        
+        For more Linear Regression info, you can view it here: https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.ElasticNet.html#sklearn.linear_model.ElasticNet 
+        If running gridsearch, the implemented cross validators are:
+            - 'kfold' for KFold
+            - 'strat-kfold' for StratifiedKfold
+ 
+        Possible scoring metrics: 
+            - ‘explained_variance’ 	 
+            - ‘max_error’ 	 
+            - ‘neg_mean_absolute_error’ --> MAE	 
+            - ‘neg_mean_squared_error’ --> MSE 	 
+            - ‘neg_mean_squared_log_error’ --> MSLE
+            - ‘neg_median_absolute_error’ --> MeAE 	 
+            - ‘r2’
+        
+        Parameters
+        ----------
+        cv : bool, optional
+            If True run crossvalidation on the model, by default None.
+        gridsearch : int, Crossvalidation Generator, optional
+            Cross validation method, by default None
+        score : str, optional
+            Scoring metric to evaluate models, by default ‘neg_mean_squared_error’
+        model_name : str, optional
+            Name for this model, by default "elastic"
+        run : bool, optional
+            Whether to train the model or just initialize it with parameters (useful when wanting to test multiple models at once) , by default False
+        verbose : int, optional
+            Verbosity level of model output, the higher the number - the more verbose. By default, 1   
+        
+        alpha : float, optional
+            Constant that multiplies the penalty terms.
+            Defaults to 1.0. See the notes for the exact mathematical meaning of this parameter.
+            ``alpha = 0`` is equivalent to an ordinary least square, solved by the LinearRegression object.
+            For numerical reasons, using alpha = 0 with the Lasso object is not advised.
+            Given this, you should use the LinearRegression object.
+        l1_ratio : float
+            The ElasticNet mixing parameter, with 0 <= l1_ratio <= 1.
+            For l1_ratio = 0 the penalty is an L2 penalty.
+            For l1_ratio = 1 it is an L1 penalty.
+            For 0 < l1_ratio < 1, the penalty is a combination of L1 and L2.
+        fit_intercept : bool
+            Whether the intercept should be estimated or not.
+            If False, the data is assumed to be already centered.
+        normalize : boolean, optional, default False
+            This parameter is ignored when fit_intercept is set to False.
+            If True, the regressors X will be normalized before regression by subtracting the mean and dividing by the l2-norm.
+            If you wish to standardize, please use sklearn.preprocessing.
+        precompute : True | False | array-like
+            Whether to use a precomputed Gram matrix to speed up calculations.
+            The Gram matrix can also be passed as argument.
+            For sparse input this option is always True to preserve sparsity.
+        max_iter : int, optional
+            The maximum number of iterations
+        tol : float, optional
+            The tolerance for the optimization: if the updates are smaller than tol, the optimization code checks the dual gap for optimality and continues until it is smaller than tol.
+        
+        positive : bool, optional
+            When set to True, forces the coefficients to be positive.
+        selection : str, default ‘cyclic’
+            If set to ‘random’, a random coefficient is updated every iteration rather than looping over features sequentially by default.
+            This (setting to ‘random’) often leads to significantly faster convergence especially when tol is higher than 1e-4.
+        Returns
+        -------
+        RegressionModelAnalysis
+            RegressionModelAnalysis object to view results and analyze results
+        Examples
+        --------
+        >>> model.ElasticNetRegression()
+        >>> model.ElasticNetRegression(model_name='m1', alpha=0.0003)
+        >>> model.ElasticNetRegression(cv=10)
+        >>> model.ElasticNetRegression(gridsearch={'alpha':[0.01, 0.02]}, cv='strat-kfold')
+        >>> model.ElasticNetRegression(run=False) # Add model to the queue
+        """
+        # endregion
+
+        from sklearn.linear_model import ElasticNet
+
+        model = ElasticNet
+
+        model = self._run_supervised_model(
+            model,
+            model_name,
+            RegressionModelAnalysis,
+            cv_type=cv_type,
+            gridsearch=gridsearch,
+            score=score,
+            run=run,
+            verbose=verbose,
+            **kwargs,
+        )
+
+        return model
+
