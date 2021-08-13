1131b34d996bccb3bf827c7fcb5e3ee2635b8c74
Karthik <karthikrajabk@gmail.com>
2021-08-13 09:11:38 -0400

    init for modelling pipeline

---
+++ nyx/modelling/__init__.py
@@ -0,0 +1,10 @@
+from .classification_models import Classification
+from .regression_models import Regression
+from .unsupervised_models import Unsupervised
+
+
+__all__ = [
+    "Classification",
+    "Regression",
+    "Unsupervised",
+]
