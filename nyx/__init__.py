10919daefcd8ef7efc99ed7b5bab02ad397e00f7
Karthik <karthikrajabk@gmail.com>
2021-08-13 09:18:01 -0400

    __init__ done

--- nyx/__init__.py
+++ nyx/__init__.py
@@ -19,3 +19,47 @@ if not os.path.exists(config_home):
 import pandas as pd
 from IPython import get_ipython
 import plotly.io as pio
+
+# let init-time option registration happen
+import nyx.config.config_init
+from nyx.config.config import (
+    describe_option,
+    get_option,
+    options,
+    reset_option,
+    set_option,
+)
+
+from nyx.helpers import groupby_analysis
+
+from nyx.analysis import Analysis
+from nyx.modelling import Classification, Regression, Unsupervised
+from nyx.model_analysis import (
+    ClassificationModelAnalysis,
+    RegressionModelAnalysis,
+    UnsupervisedModelAnalysis,
+    TextModelAnalysis,
+)
+
+pd.options.mode.chained_assignment = None
+pio.templates.default = "plotly_white"
+
+__all__ = [
+    "Analysis",
+    "Classification",
+    "Regression",
+    "Unsupervised",
+    "ClassificationModelAnalsysis",
+    "RegressionModelAnalysis",
+    "UnsupervisedModelAnalysis",
+    "TextModelAnalysis",
+]
+
+shell = get_ipython().__class__.__name__
+
+if shell == "ZMQInteractiveShell":
+    import shap
+    from plotly.offline import init_notebook_mode
+
+    init_notebook_mode(connected=True)
+    shap.initjs()
