a740c095953db08a947d1c8f3af72e300e54fc7a
Karthik <karthikrajabk@gmail.com>
2021-08-13 09:04:11 -0400

    Unsupervised Class - init function

--- nyx/modelling/unsupervised_models.py
+++ nyx/modelling/unsupervised_models.py
@@ -10,3 +10,23 @@ from nyx.feature_engineering.feature import Feature
 from nyx.visualizations.visualizations import Visualizations
 from nyx.stats.stats import Stats
 from nyx.modelling.util import add_to_queue
+
+class Unsupervised(
+    ModelBase, Analysis, Clean, Preprocess, Feature, Visualizations, Stats
+):
+    def __init__(
+        self, x_train, exp_name="my-experiment",
+    ):
+        """
+        Class to run analysis, transform your data and run Unsupervised algorithms.
+        Parameters
+        -----------
+        x_train: pd.DataFrame
+            Training data or aethos data object
+        exp_name : str
+            Experiment name to be tracked in MLFlow.
+        """
+
+        super().__init__(
+            x_train, "", x_test=None, test_split_percentage=0.2, exp_name=exp_name,
+        )
