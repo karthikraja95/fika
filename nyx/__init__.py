66b66375208dfda2ecd2e6a59c1a4bfc36a4257d
Karthik <karthikrajabk@gmail.com>
2021-08-13 09:15:50 -0400

    adding relavant directories path

--- nyx/__init__.py
+++ nyx/__init__.py
@@ -4,3 +4,8 @@ import shutil
 
 warnings.simplefilter("ignore", FutureWarning)
 warnings.simplefilter("ignore", DeprecationWarning)
+
+nyx_home = os.path.join(os.path.expanduser("~"), ".nyx")
+config_home = os.path.join(nyx_home, "config.yml")
+pkg_directory = os.path.dirname(__file__)
+
